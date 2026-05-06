1d08a61573520c6718d77c90f30d1d56e4c9618a
Zhangmou <2783725491@qq.com>
2026-05-07 03:01:38 +0800

    基本完善

--- 右侧预览.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0tools\open-preview-right.ps1"
--- 启动预览服务.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-"D:\nodejs\node.exe" "%~dp0preview-server.js"
-pause
